daefc4a22b252d14e27e1343ffa6975b84be280b
Miao Chang Xin <miaocx@cn.ibm.com>
2018-10-24 18:05:40 +0800

    Add cli interface for easy interacting with zos

---
+++ bin/zos-cli.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+var Client = require('../lib/zosAccessor');
+
+var argv = process.argv.slice(2),
+    connectOptions = {},
+    operations = [];
+for (var i=0; i < argv.length; i+=1) {
+    var arg = argv[i];
+    if (arg.startsWith('--')) {
+        connectOptions[arg.substr('--'.length)] = argv[i+1];
+        i += 1;
+    } else {
+        operations.push(arg);
+    }
+}
+
+if (Object.keys(connectOptions).length === 0) {
+    console.error('Usage: zos [connect-options] method ...method-arguments');
+    console.error('\nconnect-options are arguments passed to the "connect" method, each should start with "--".');
+    console.error("\neg. zos --host MYSERVER --user ABC --password SECRET submitJCL /path/to/local.JCL");
+    process.exit(0);
+}
+
+new Client().connect(connectOptions).then(function (connection) {
+    if (operations.length) {
+        return connection[operations[0]].apply(connection, operations.slice(1));
+    }
+    return 'Connect successfully';
+}).then(function(resp) {
+    resp = resp || 'OK';
+    if (typeof resp === 'string') {
+        console.log(resp);
+    } else if (Buffer.isBuffer(resp)) {
+        console.log(resp.toString());
+    } else {
+        console.log(JSON.stringify(resp, null, 2));
+    }
+    process.exit(0);
+}).catch(function(err) {
+    console.error(err.message);
+    process.exit(1);
+});
